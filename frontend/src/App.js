bf1d36aafdf4f6d3df0681352d8dd8ac7e8ab487
OctoM <gersikblock@outlook.com>
2021-05-09 04:00:41 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Text">
+          <p>
+            This is frontend for MPIT2021.
+          </p>
+          <p style={{fontWeight: "bold", fontSize: "calc(12px + 2vmin)"}}>
+            Created by 3Limbs
+          </p>
+        </div>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
